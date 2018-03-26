e3b302b1e1f72958ccd2fe21421cbb61c8bcc2d0
anthony <anthony.chinwo@gmail.com>
2018-03-26 15:36:22 +0100

    - switch bkendz

--- server.js
+++ server.js
@@ -2,109 +2,29 @@
  * Created by anthony on 02/02/2018.
  */
 
-const http = require('http')
-const WebSocketServer = require('websocket').server;
-const messageHandler = require('./message_handler')
-const _ = require('lodash');
+const _ = require('lodash')
+const bz = require('bkendz')
+const path = require('path')
 
-// Serve up public/ftp folder
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-
-const app = express()
-
-app.set('view engine', 'ejs')
-app.use(logger('":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-
-app.use('/', messageHandler.routeHandler)
-
-app.use(express.static(path.join(__dirname, './src')))
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+const app = new bz.Bkendz({
+    administerEnabled: true,
+    clientEnabled: false,
+    apiEnabled: false,
+    optsAdmin: {staticPath: path.join(__dirname, './src')}
 })
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
-    
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
-    console.error(err)
+app.admin.on('request', (messageHandler, request, conn) => {
+    messageHandler.respond(conn, request)
 })
 
-module.exports = app;
+app.admin.messageHandlers.ws.topic('/status', () => {
+    return {data: {clients: app.admin.connections.length}}
+})
 
-const server = http.createServer(app)
-// Listen
+app.admin.messageHandlers.http.get('/', (req, res) => {
+    res.render('index')
+})
 
-const port = process.env.PORT || 26116
+const port = process.env.PORT || 8081
 console.log(`starting monitoring server on ${port}...`)
-server.listen(port)
-
-const wsServer = new WebSocketServer({httpServer: server, autoAcceptConnections: false});
-
-function originIsAllowed(origin) {
-    // put logic here to detect whether the specified origin is allowed.
-    return true;
-}
-
-wsServer.on('request', function (request) {
-    if (!originIsAllowed(request.origin)) {
-        // Make sure we only accept requests from an allowed origin
-        request.reject();
-        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-        return;
-    }
-    console.dir('websocket:', request)
-    
-    const connection = request.accept('echo-protocol', request.origin);
-    
-    connection._resourceURL = request.resourceURL
-    console.log((new Date()) + ' Connection accepted.');
-    
-    connection.on('message', function(msg){
-        let resp = messageHandler.wsHandler.onMessage(this, {msg:JSON.parse(msg.utf8Data), type:msg.type, raw:msg})
-        
-        switch(resp.type){
-            case 'utf8':
-                let dataStr = resp.data
-    
-                if (!_.isString(dataStr)) {
-                    dataStr = JSON.stringify(dataStr);
-                }
-    
-                this.sendUTF(dataStr, (error) => {
-                    if (error) {
-                        console.error(error)
-                    } else {
-                        console.log('message sent:', resp.data)
-                    }
-                })
-                break;
-            case 'binary':
-                this.sendBytes(resp.binaryData);
-                break;
-            default:
-                throw Error(`unknown response data type ${resp.dataType}`)
-        }
-    
-    })
-    
-    connection.on('close', function(reasonCode, description){
-        messageHandler.wsHandler.onClose(this, reasonCode, description)
-    })
-    
-})
+app.listen(port)
